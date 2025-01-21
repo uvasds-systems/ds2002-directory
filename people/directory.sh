aac419c5004b754374c6b92d33be61b1194512ae
Neal Magee <nmagee@users.noreply.github.com>
2025-01-21 19:52:13 +0000

    Set up git things

--- people/directory.sh
+++ people/directory.sh
@@ -5,11 +5,13 @@
 # Blow away the README and rebuild
 touch README.md
 
+# readme boilerplate
 echo "# ds2002-directory" > README.md
 echo " " >> README.md
 echo "A people directory for DS2002" >> README.md
 echo " " >> README.md
 
+# fetch name of each person and set up tree
 for dir in */; do
     if [ -d "$dir" ]; then
         NAME=`head -1 $dir/README.md | sed -e 's/#//g;'`
@@ -17,7 +19,12 @@ for dir in */; do
     fi
 done
 
+# move to base dir
 mv README.md ../README.md
+
+# git things and commit
+git config --global user.email "nem2p@virginia.edu"
+git config --global user.name "N Magee"
 
 cd ..
 git add .
