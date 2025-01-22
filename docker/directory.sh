66a8a2dcf2d95da8a230dbd850ad3d36e1bcb467
N Magee <nem2p@virginia.edu>
2025-01-22 13:46:02 -0500

    Let it go

---
+++ docker/directory.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -evx
+
+cd ds2002-directory/people/
+
+# Blow away the README and rebuild
+touch README.md
+
+# readme boilerplate
+echo "# ds2002-directory" > README.md
+echo " " >> README.md
+echo "A people directory for DS2002" >> README.md
+echo " " >> README.md
+
+# fetch name of each person and set up tree
+for dir in */; do
+    if [ -d "$dir" ]; then
+        NAME=`head -1 $dir/README.md | sed -e 's/#//g;'`
+        echo "- [$NAME](people/$dir)" >> README.md
+    fi
+done
+
+# move to base dir
+mv README.md ../README.md
+
+cd ..
+
+git config --global user.email "nem2p@virginia.edu"
+git config --global user.name "N Magee"
+git commit -am "updated directory via merge"
+git push origin main
